e9aa12ebf3e8a71c9e5168c9249924c677abd2c7
Matthew Sanders <msanders@lightmaregaming.net>
2017-07-07 20:55:58 -0400

    update meta and icon

--- quora-fix.user.js
+++ quora-fix.user.js
@@ -4,10 +4,11 @@
 // @author       msanders-lg
 // @version      0.15
 // @description  Removes login wall + scroll lock
+// @iconURL     https://raw.githubusercontent.com/msanders-lg/quora-fix/master/logo.png
 // @contactURL   https://github.com/msanders-lg
 // @supportURL   https://github.com/msanders-lg
 // @downloadURL  https://raw.githubusercontent.com/msanders-lg/quora-fix/master/quora-fix.user.js
-// @updateURL    https://raw.githubusercontent.com/msanders-lg/quora-fix/master/quora-fix.user.js
+// @updateURL    https://openuserjs.org/meta/msanders-lg/Quora_Fix.meta.js
 // @license      MIT; https://raw.githubusercontent.com/msanders-lg/quora-fix/master/license
 // @include      http*://*.quora.com/*
 // @require      https://code.jquery.com/jquery-3.2.1.slim.min.js#md5=5f48fc77cac90c4778fa24ec9c57f37d,sha256=9365920887b11b33a3dc4ba28a0f93951f200341263e3b9cefd384798e4be398
